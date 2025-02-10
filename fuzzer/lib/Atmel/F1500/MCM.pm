b8fc025b580605b46e0db42c9f31ad847c5c3f57
Alexei A. Smekalkine <ikle@ikle.ru>
2025-02-10 08:14:11 +0300

    Atmel::F1500: add MC position mapping helper module

---
+++ fuzzer/lib/Atmel/F1500/MCM.pm
@@ -0,0 +1,136 @@
+#
+# Macro Cell Position Mapping for Atmel ATF1500 family
+#
+# Copyright (c) 2025 Alexei A. Smekalkine <ikle@ikle.ru>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+package Atmel::F1500::MCM;
+
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw (Exporter);
+
+our @EXPORT = qw (
+	mcm_read_jed
+	mcm_alloc
+	mcm_load
+	mcm_save
+	mcm_report
+	mcm_update
+);
+
+use CVS::Table;
+
+#
+# Reads MC position mapping
+#
+# Returns a mapping from a LAB name to a mapping from switch pair name to
+# the address in JEDEC file
+#
+sub mcm_read_jed ($) {
+	my ($name) = @_;
+	my %data;
+
+	open my $jed, '<', "$name.jed" or die "E: Cannot open $name.jed\n";
+
+	for my $line (<$jed>) {
+		if ($line =~ /^L(\d+) [01]+\*\s*NOTE S(\d+)\s*,\s*S(\d+)\s+of block ([A-Z])/) {
+			$data{$4}{"S$2,S$3"} = $1;
+		}
+	}
+
+	return \%data;
+}
+
+#
+# Creates a table of `cols' columns and `rows' rows filled with the
+# default value (hyphen -- unknown position).
+#
+sub mcm_alloc ($$) {
+	my ($cols, $rows) = @_;
+
+	return table_alloc ($cols, $rows, '-');
+}
+
+#
+# Loads MC position map from the specified CSV file. If the file cannot be
+# opened, returns an empty map.
+#
+sub mcm_load ($$$) {
+	my ($cols, $rows, $path) = @_;
+
+	return table_load ($cols, $rows, $path, '-');
+}
+
+#
+# Saves the MC position map to the specified file.
+#
+sub mcm_save ($$) {
+	my ($table, $path) = @_;
+
+	return table_save ($table, $path);
+}
+
+#
+# Generates and prints a report of the known MC position map and its
+# coverage.
+#
+sub mcm_report ($;$$) {
+	my ($table, $prefix, $suffix) = @_;
+
+	return table_report ($table, '-', $prefix, $suffix);
+}
+
+#
+# Updates MC position mapping
+#
+sub mcm_update ($$) {
+	my ($table, $jed) = @_;
+	my $rows = scalar @{$table};
+	my $cols = scalar @{$table->[0]};
+
+	my %map = (
+		'S16,S12' => 0,
+		'S14,S11' => 1,
+		'S9,S6'   => 2,
+		'S13,S10' => 3,
+		'S20,S18' => 4,
+		'S8,S21'  => 5,
+		'S7,S19'  => 6,
+		'S22,S5'  => 7,
+		'S23,S4'  => 8,
+		'S3,S15'  => 9,
+		'S0,S1'   => 10,
+		'S17,S2'  => 11,
+	);
+
+	for my $lab (keys %{$jed}) {
+		my $i = ord ($lab) - ord ('A');
+
+		die "E: To few rows in MC position mapping\n" if $i >= $rows;
+
+		for my $spair (keys %{$jed->{$lab}}) {
+			my $sp = $map{$spair};
+
+			die "E: Unknown switch pair $spair\n" unless defined $sp;
+			die "E: To few columns in MC position mapping\n" if $sp >= $cols;
+
+			my $old = $table->[$i][$sp];
+			my $new = $jed->{$lab}{$spair};
+
+			next if ($new eq '-' or $old eq $new);
+
+			die "E: MC mapping conflict for LAB $lab, pair $spair\n"
+			unless ($old eq '-');
+
+			$table->[$i][$sp] = $new;
+		}
+	}
+}
+
+1;
--- fuzzer/lib/Atmel/F1502.pm
+++ fuzzer/lib/Atmel/F1502.pm
@@ -15,6 +15,7 @@ require Exporter;
 
 our @ISA = qw (Exporter);
 
+use Atmel::F1500::MCM;
 use Atmel::F1500::PIM;
 use Atmel::F1500::PTC;
 use Atmel::F1500::PTM;
@@ -38,6 +39,7 @@ sub f1502_alloc () {
 
 	$o{'pim'} = pim_alloc (2, 40);
 	$o{'ptm'} = ptm_alloc (5, 32);
+	$o{'mcm'} = ptm_alloc (12, 2);
 
 	$o{'ptc'} = ptc_alloc (96);
 	$o{'uim'} = uim_alloc (5, 40);
@@ -51,6 +53,7 @@ sub f1502_load ($) {
 
 	$o{'pim'} = pim_load (2, 40, "$db/atmel/f1502/pim");
 	$o{'ptm'} = ptm_load (5, 32, "$db/atmel/f1502/ptm");
+	$o{'mcm'} = mcm_load (12, 2, "$db/atmel/f1502/mcm");
 
 	$o{'ptc'} = ptc_load (96,    "$db/atmel/f1502/ptc");
 	$o{'uim'} = uim_load (5, 40, "$db/atmel/f1502/uim");
@@ -63,6 +66,7 @@ sub f1502_save ($$) {
 
 	pim_save ($o->{'pim'}, "$db/atmel/f1502/pim");
 	ptm_save ($o->{'ptm'}, "$db/atmel/f1502/ptm");
+	mcm_save ($o->{'mcm'}, "$db/atmel/f1502/mcm");
 
 	ptc_save ($o->{'ptc'}, "$db/atmel/f1502/ptc");
 	uim_save ($o->{'uim'}, "$db/atmel/f1502/uim");
@@ -73,6 +77,7 @@ sub f1502_report ($) {
 
 	pim_report ($o->{'pim'}, "# UIM Position Mapping\n\n", "\n");
 	ptm_report ($o->{'ptm'}, "# PT Position Mapping\n\n", "\n");
+	mcm_report ($o->{'mcm'}, "# MC Position Mapping\n\n", "\n");
 
 	ptc_report ($o->{'ptc'}, "# PT Configuration\n\n", "\n");
 	uim_report ($o->{'uim'}, "# UIM Mapping\n\n");
@@ -83,6 +88,8 @@ sub f1502_update ($$) {
 
 	pim_update ($o->{'pim'},        pim_read_jed (       $path));
 	ptm_update ($o->{'ptm'},        ptm_read_jed (       $path));
+	mcm_update ($o->{'mcm'},        mcm_read_jed (       $path));
+
 	uim_update ($o->{'uim'}, $path, uim_read_jed (5, 40, $path));
 }
 
